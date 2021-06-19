2ffe61517f02b97aee489f143125d5adc68dbcfa
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2021-06-20 00:45:29 +0300

    3-part

--- gulpfile.js
+++ gulpfile.js
@@ -6,44 +6,45 @@ const rename = require("gulp-rename");
 const browserSync = require("browser-sync").create();
 var svgSprite = require("gulp-svg-sprite");
 
+
 function style() {
-    return gulp
-        .src("./scss/**/*.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(rename({ dirname: "./css", suffix: ".min" }))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest("./"))
-        .pipe(browserSync.stream());
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(rename({ dirname: "./css", suffix: ".min" }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("./"))
+    .pipe(browserSync.stream());
 }
 
 gulp.task("svgSprite", function () {
-    return gulp
-        .src("images/svg/*.svg") // svg files for sprite
-        .pipe(
-            svgSprite({
-                mode: {
-                    stack: {
-                        sprite: "../sprite.svg", //sprite file name
-                    },
-                },
-            })
-        )
-        .pipe(gulp.dest("images/sprite"));
+  return gulp
+    .src("images/svg/*.svg") // svg files for sprite
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: "../sprite.svg", //sprite file name
+          },
+        },
+      })
+    )
+    .pipe(gulp.dest("images/sprite"));
 });
 
 gulp.task("html", function () {
-    return gulp.src("*.html").pipe(browserSync.reload({ stream: true }));
+  return gulp.src("*.html").pipe(browserSync.reload({ stream: true }));
 });
 
 function watch() {
-    browserSync.init({
-        server: {
-            baseDir: "./",
-        },
-    });
-    gulp.watch("./scss/*.scss", gulp.parallel("style"));
-    gulp.watch("./*.html", gulp.parallel("html"));
-    gulp.watch("./js/*.js").on("change", browserSync.reload);
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  gulp.watch("./scss/*.scss", gulp.parallel("style"));
+  gulp.watch("./*.html", gulp.parallel("html"));
+  gulp.watch("./js/*.js").on("change", browserSync.reload);
 }
 exports.style = style;
 exports.watch = watch;
